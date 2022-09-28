1b546eb69689ffbaf91cebdb43b3f664c6927703
Jacob Luke Russell <jluke.russell@gmail.com>
2022-09-27 18:17:25 -0600

    Finalized Game

--- Program.cs
+++ Program.cs
@@ -86,6 +86,15 @@
         else if (board[0] == player && board[4] == player && board[8] == player){
             return true;
         }
+        else if (board[0] == player && board[3] == player && board[6] == player){
+            return true;
+        }
+        else if (board[1] == player && board[4] == player && board[7] == player){
+            return true;
+        }
+        else if (board[2] == player && board[5] == player && board[8] == player){
+            return true;
+        }
 
 
         return false;
@@ -95,25 +104,47 @@
     /// <param name="board">The current board.</param>
     /// <returns>True if the board is full.</returns>
     static bool IsTie(List<string> board)
-    {
-        return false;
-    }
+        {
+            // If there is a digit, there are still moves to be made.
+            bool foundDigit = false;
+
+            foreach (string value in board)
+            {
+                if (char.IsDigit(value[0]))
+                {
+                    foundDigit = true;
+                    break;
+                }
+            }
+
+            return !foundDigit;
+        }
 
     /// <summary>Cycles through the players (from x to o and o to x)</summary>
     /// <param name="currentPlayer">The current players sign (x or o)</param>
     /// <returns>The next players sign (x or o)</returns>
     static string GetNextPlayer(string currentPlayer)
     {
-        return "x";
+        string nextPlayer = "x";
+        if (currentPlayer == "x")
+        {
+            nextPlayer = "o";
+        }
+        
+        return nextPlayer;
     }
-
     /// <summary>Gets the 1-based spot number associated with the user's choice.</summary>
     /// <param name="currentPlayer">The sign (x or o) of the current player.</param>
     /// <returns>A 1-based spot number (not a 0-based index)</returns>
     static int GetMoveChoice(string currentPlayer)
     {
-        return 1;
+        Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
+        string move_string = Console.ReadLine();
+
+        int choice = int.Parse(move_string);
+        return choice;
     }
+    
 
     /// <summary>
     /// Places the current players mark on the board at the desired spot.
@@ -124,6 +155,7 @@
     /// <param name="currentPlayer">The current player's sign (x or o)</param>
     static void MakeMove(List<string> board, int choice, string currentPlayer)
     {
-        
+        int index = choice - 1;
+        board[index] = currentPlayer;
     }
 }
